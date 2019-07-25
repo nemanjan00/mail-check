0ace51b143c83d985411a68b500281b58c205389
nemanjan00 <nemanjan00@gmail.com>
2019-07-25 19:48:57 +0200

    Registry

--- src/registry/index.js
+++ src/registry/index.js
@@ -1,2 +1,25 @@
-module.exports = {};
+const domainChecker = require("../domain");
+
+const domains = {};
+
+const registry = {
+	getDomain: (domain) => {
+		if(domains[domain] === undefined){
+			domains[domain] = domainChecker(domain);
+		}
+
+		return domains[domain];
+	},
+	addDomain: (domain) => {
+		return registry.getDomain[domain];
+	},
+	getDomains: () => {
+		return domains;
+	},
+	clearDomain: (domain) => {
+		delete domains[domain];
+	}
+};
+
+module.exports = registry;
 
