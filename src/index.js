eab30b2c6b1e0ba41e651d3459cd9d850478e88b
nemanjan00 <nemanjan00@gmail.com>
2019-07-25 19:44:58 +0200

    Structure

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const registry = require("./src/registry");
+const domain = require("./src/domain");
+const check = require("./src/check");
+
+module.exports = {
+	registry,
+	domain,
+	check
+};
+
